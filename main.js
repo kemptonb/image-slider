6e4dc1461a433c4356f1fe8b69e4e623d2a51e41
bradley <bradleybkempton@gmail.com>
2022-04-27 18:52:20 -0700

    Added JS logic, style, and media brake points.

--- main.js
+++ main.js
@@ -1,19 +1,60 @@
-const slideBar = document.getElementById("slideBarVar");
-let position = [0, -48, 96];
-let i = 1;
-let placeholder = `\"translate(${position[i]}em, 0)\"`;
-console.log(placeholder)
 
-document.getElementById("arrow1").addEventListener("click", ()=> {
-    
-    //slideBar.style.transform = "translate(0, 0)";
-        
-    slideBar.style.transform = placeholder;
-   
-});
+//load page
+(() => {
 
-document.getElementById("arrow2").addEventListener("click", ()=> {
-    slideBar.style.transform = "translate(-48em, 0)";
+    //module vars  
+    const slideBar = document.getElementById("slideBarVar");
 
+    //slide tracking var
+    let i = 0;
 
-});
+    //generate slide array
+    let slide = document.getElementsByClassName("slide");
+    slide[0].classList.add("seen");
+
+    //dom buttons
+    document.getElementById("arrow1").addEventListener("click", () => {
+        backArrow();
+    });
+
+    document.getElementById("arrow2").addEventListener("click", () => {
+        forwardArrow();
+    });
+
+    //arrow button logic
+    let forwardArrow = () => {
+        if (i == 0) {
+            slide[i].classList.remove("seen");
+            i++;
+            slide[i].classList.add("seen");
+        } else if (i < slide.length - 1) {
+            slide[i].classList.remove("seen");
+            i++;
+            slide[i].classList.add("seen");
+        } else if (i == 2) {
+            slide[i].classList.remove("seen");
+            i = 0;
+            slide[i].classList.add("seen");
+        }
+    }
+
+    let backArrow = () => {
+        if (i == 0) {
+            slide[i].classList.remove("seen");
+            i = slide.length - 1;
+            slide[slide.length - 1].classList.add("seen");
+        } else if (i <= slide.length - 1) {
+            slide[i].classList.remove("seen");
+            i--;
+            slide[i].classList.add("seen");
+        }
+    }
+
+    //timer function
+    (function () {
+        forwardArrow();
+        setTimeout(arguments.callee, 3000);
+    })();
+
+})();
+
